3895d22f0aa1cc3f39c41fe5f0bd64222a2043ea
Ian Magonya <ianbenard2002@gmail.com>
2024-07-08 21:51:25 +0300

    no error

--- api/index.js
+++ api/index.js
@@ -5,12 +5,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('Connected to MongoDB!');
-}).catch((err)=>{
-    console.log(err)
+}).catch((err) => {
+    console.log('Failed to connect to MongoDB!', err);
 })
+
 
 const app = express();
 
 app.listen(3000, () => {
     console.log('Listening on port 3000!');
+});
+
+app.get('/test', (req, res) => {
+    res.send('Hello World!');
 });
